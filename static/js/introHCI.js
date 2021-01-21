bf6afc08201c1582ebe1c24952b6a09cd222ec84
Lydia Smith <l8smith@ucsd.edu>
2021-01-21 13:38:00 -0500

    added JS

--- static/js/introHCI.js
+++ static/js/introHCI.js
@@ -4,15 +4,36 @@
 $(document).ready(function() {
 	initializePage();
 })
+console.log("some string");
 
 /*
  * Function that is called when the document is ready.
  */
 function initializePage() {
 	$("#testjs").click(function(e) {
-		$('.jumbotron h1').text("Javascript is connected");
+		$('.jumbotron h1').text("Javascript has taken control");
+		$(".jumbotron p").toggleClass("active");
+		$("#testjs").text("Please wait ...");
 	});
 
 	// Add any additional listeners here
 	// example: $("#div-id").click(functionToCall);
+$("a.thumbnail").click(projectClick);
+function projectClick(e) {
+	console.log("project clicked");
+	e.preventDefault();
+	$(this).css("background-color", "#9332a8");
+	//var containingProject = $(this).closest(".project");
+    //containingProject.append("<div class='project-description'><p>Description of the project.</p></div>");
+    var containingProject = $(this).closest(".project");
+    var description = $(containingProject).find(".project-description");
+    if (description.length == 0) {
+       $(containingProject).append("<div class='project-description'><p>Description of the project.</p></div>");
+    } else {
+       description.html("<p>Stop clicking on me! You just did it at " + (new Date()) + "</p>");
+    }
+    $(description).fadeOut()
+}
+
+
 }
